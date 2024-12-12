0d960d12c69f9c1ac315b055db54bc0ec1c52310
Awdziewiczka <liluter@MacBook-Pro-Awdziewiczka.local>
2024-12-12 20:53:33 +0100

    Configured firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface Item {
+  name: string,
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [AsyncPipe],
+  template: `
+  <p>Testing...</p>
+  <ul>
+    @for (item of item$ | async; track item) {
+      <li>
+        {{ item.name }}
+      </li>
+    }
+  </ul>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'factory-manager';
+  item$: Observable<Item[]>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'items');
+    this.item$ = collectionData<Item>(itemCollection);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzR2EbaT-ic9VxfXaY6-n4vs2FIBMqXqQ",
+    authDomain: "factory-manager-c71f8.firebaseapp.com",
+    projectId: "factory-manager-c71f8",
+    storageBucket: "factory-manager-c71f8.firebasestorage.app",
+    messagingSenderId: "119966284443",
+    appId: "1:119966284443:web:f26c7c83d9c411cfdca43e"
+  }
+}

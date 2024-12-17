9e839864ecc1e35f004f83418bdd96fe77d12d3b
Karol <karolawdziewicz@gmail.com>
2024-12-17 17:33:40 +0100

    Tweak firestore .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,51 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject, Signal } from '@angular/core';
 import { collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { NavbarComponent } from "./components/UI/navbar/navbar.component";
-
+import { toSignal } from '@angular/core/rxjs-interop'
+import { loggedIn } from '@angular/fire/auth-guard';
 interface Item {
   name: string,
 };
 
+interface Link {
+  list: string[],
+};
+interface User {
+  name: string
+  loggedIn: boolean
+  links: string[]
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, NavbarComponent],
+  imports: [AsyncPipe, NavbarComponent, JsonPipe],
   template: `
-  <app-navbar [links]="['Dashboard','Workshop']">Factory Manager</app-navbar>
-  <p>Testing...</p>
-  <ul>
-    @for (item of item$ | async; track item) {
-      <li>
-        {{ item.name }}
-      </li>
+    @for (user of logged(); track user) {
+      <app-navbar [links]="user.links">Factory Manager  - {{user.name}}</app-navbar>
     }
-
-
-  </ul>
   `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   item$: Observable<Item[]>;
   firestore: Firestore = inject(Firestore);
-
+  // link$: Observable<Link[]>;
+  user$: Observable<User[]>;
+  logged$: Observable<User[]>;
+  // links: Signal<Link[]>
+  logged: Signal<User[]>
   constructor() {
     const itemCollection = collection(this.firestore, 'items');
+    // const linkCollection = collection(this.firestore, 'links');
+    const userCollection = collection(this.firestore, 'users')
     this.item$ = collectionData<Item>(itemCollection);
+    // this.link$ = collectionData<Link>(linkCollection);
+    this.user$ = collectionData<Link>(userCollection);
+    this.logged$ = this.user$.pipe(map(data => data.filter(user => user.loggedIn === true)))
+    // this.links = toSignal(this.link$, { initialValue: [{ list: [] }] })
+    this.logged = toSignal(this.logged$, { initialValue: [{ name: '', loggedIn: false, links: [] }] })
 
   }
 
--- src/app/components/UI/navbar/navbar.component.ts
+++ src/app/components/UI/navbar/navbar.component.ts
@@ -3,17 +3,17 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   template: `
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+  <div class="container-fluid ">
     <span class="navbar-brand mb-0 h1"><ng-content ></ng-content>
     </span>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <ul class="navbar-nav">
         @for(link of links; track link){
-        <li class="nav-item">
+        <li class="nav-item me-3">
           <a class="nav-link" href="#">{{link}}</a>
         </li>
         }

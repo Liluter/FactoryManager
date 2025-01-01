276838d4dc63b38af3a75f1278cf545534913922
Awdziewiczka <liluter@MacBook-Pro-Awdziewiczka.local>
2025-01-02 00:15:31 +0100

    Add current route url to navbar.

--- src/app/components/UI/navbar/navbar.component.ts
+++ src/app/components/UI/navbar/navbar.component.ts
@@ -1,21 +1,31 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, Input } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { UserService } from '../../../services/user.service';
+import { AsyncPipe } from '@angular/common';
+import { filter, map } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   standalone: true,
-  imports: [RouterModule]
+  imports: [RouterModule, AsyncPipe]
 })
 export class NavbarComponent {
   @Input() links: string[] = ['login']
   userService: UserService = inject(UserService)
+  private router: Router = inject(Router)
+  currentUrl$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map((event: NavigationEnd) => event.url.replace('/', 'on '))
+  )
+
   async routeTo(link: string) {
     if (link === 'logout') {
       await this.userService.logAuthOut()
       console.log('Logged Out')
-
     }
   }
+
 }

c3107f474c292f5a5da844eff8b59f4b17e73f00
Liluter <karolawdziewicz@gmail.com>
2025-01-05 17:56:25 +0100

    refactor app ocmponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { AsyncPipe } from '@angular/common';
-import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
-import { delay, distinctUntilChanged, filter, map, Observable, switchMap, tap } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { NavbarComponent } from "./components/UI/navbar/navbar.component";
-import { ActivatedRoute, Data, NavigationEnd, Router, RouterModule, RouterOutlet, UrlSegment } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { UserService } from './services/user.service';
-import { User } from 'firebase/auth';
-import { FSUser, LocalUser } from './types/auth.interface';
+import { FSUser } from './types/auth.interface';
 import { RoutesService } from './services/routes.service';
-
-interface Item {
-  name: string,
-};
-
-interface Link {
-  list: string[],
-};
 
 @Component({
   selector: 'app-root',
@@ -24,33 +15,22 @@ interface Link {
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  userService: UserService = inject(UserService)
   links: string[] = ['login']
-  // allUsers$: Observable<Users[]> = this.userService.getAllUsers()
-  // loggedUser$: Observable<User[] | null> = this.userService.getLoggedInUsers()
+  userService: UserService = inject(UserService)
+  private routesServise: RoutesService = inject(RoutesService)
+  currentUrl$ = this.routesServise.currenttUrl$
   loggedUser$: Observable<FSUser | null> = this.userService.userSubject$.pipe(
     tap(user => {
-      console.log('W app component', user)
       if (user?.role === 'standard') {
         this.links = ['user', 'dashboard', 'logout']
       } else if (user?.role === 'admin') {
         this.links = ['user', 'dashboard', 'settings', 'logout']
-        return
-      }
-      if (user?.links) {
+      } else if (user?.links) {
         this.links = user?.links
-        return
+      } else {
+        this.links = ['login']
       }
+      return
     })
   )
-
-  private routesServise: RoutesService = inject(RoutesService)
-  currentUrl$ = this.routesServise.currenttUrl$
-
-  logOut(id: string) {
-    // this.userService.logOut(id)
-  }
-  logOutAll() {
-    // this.userService.logOutAll().then(() => console.log('wylogowano'))
-  }
 }

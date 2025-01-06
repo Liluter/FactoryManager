8f8c3a42d2782f2dc183349d050ce7ab00bae780
Liluter <karolawdziewicz@gmail.com>
2025-01-06 14:57:11 +0100

    Add size change feature to avatar-image.

--- src/app/components/UI/avatar-image/avatar-image.component.ts
+++ src/app/components/UI/avatar-image/avatar-image.component.ts
@@ -1,14 +1,26 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatar-image',
   standalone: true,
   imports: [NgClass],
   templateUrl: './avatar-image.component.html',
-  styleUrl: './avatar-image.component.scss'
+  styleUrl: './avatar-image.component.scss',
 })
-export class AvatarImageComponent {
+export class AvatarImageComponent implements OnInit {
   @Input() id: string = '0'
   @Input() selected: boolean = false
+  @Input() size: 's' | 'm' | 'l' | 'xl' = 's'
+  @Input() hoverable: boolean = false
+  @HostBinding('style.--size') mySize = 'var(--size-s)'
+  @HostBinding('style.--scale') myScale = 'var(--size-s)'
+  ngOnInit() {
+    switch (this.size) {
+      case 's': this.mySize = 'var(--size-s)'; this.myScale = 'var(--scale-s)'; break
+      case 'm': this.mySize = 'var(--size-m)'; this.myScale = 'var(--scale-m)'; break
+      case 'l': this.mySize = 'var(--size-l)'; this.myScale = 'var(--scale-l)'; break
+      case 'xl': this.mySize = 'var(--size-xl)'; this.myScale = 'var(--scale-xl)'; break
+    }
+  }
 }

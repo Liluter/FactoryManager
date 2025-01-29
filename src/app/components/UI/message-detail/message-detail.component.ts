b23a47cdc314e2ea85349355442041f2cd0c2d32
Liluter <karolawdziewicz@gmail.com>
2025-01-29 01:36:02 +0100

    Refactor message detail component use data input as config.

--- src/app/components/UI/message-detail/message-detail.component.ts
+++ src/app/components/UI/message-detail/message-detail.component.ts
@@ -1,17 +1,24 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, Signal } from '@angular/core';
+import { Message } from '../../../types/message.interface';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-message-detail',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './message-detail.component.html',
   styleUrl: './message-detail.component.scss'
 })
 export class MessageDetailComponent {
-  id = input<string>()
+  data = input<Message | undefined>()
+  dataKeys: Signal<(string | string[] | undefined)[][]> = computed(() => [
+    [this.data()?.sender, 'Author'],
+    [this.data()?.departments, 'Departments'],
+    [this.data()?.title, 'Title'],
+    [this.data()?.message, 'Message'],
+    [this.data()?.timestamp.toDate().toISOString().toString(), 'Created'],
+    [this.data()?.id, 'Id'],
+  ])
 
-  // pobrać dane z servisu wiadomości na podstawie id . Get one by ID. i wyświetlić 
-  // opcje tytuł , content, 
-  // odpowiedz , przekaz , 
-  // jako card plus form
+  // - odpowiedz , przekaz ,  
 }
